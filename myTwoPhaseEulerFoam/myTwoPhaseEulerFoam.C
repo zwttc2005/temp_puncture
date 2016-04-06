0ae9ebfc6b0de4015256aeb9d9a3bbed7ab973e4
zwttc2005 <zwttc2005@gmail.com>
2016-04-06 11:22:53 +0100

    modified:   myTwoPhaseEulerFoam/NSCBCpuncture.H 	modified:   myTwoPhaseEulerFoam/myTwoPhaseEulerFoam.C 	modified:   myTwoPhaseEulerFoam/transportProperties.H

--- myTwoPhaseEulerFoam/myTwoPhaseEulerFoam.C
+++ myTwoPhaseEulerFoam/myTwoPhaseEulerFoam.C
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
     dimensionedScalar TwallInit
  	("TwallInit",
 	 dimensionSet (0,0,0,1,0,0,0),
-	 scalar(294.0));
+	 scalar(292.68));
     
     // friction factor
     scalar frictionFactor = 0.005;
